f0aa4e75598670a1df4b7e18eea47d4dcdd2a08a
Rohan Chopra <09rohanchopra@gmail.com>
2017-01-12 00:50:28 +0530

    Migrated to Mariadb

--- vitbot/settings.py
+++ vitbot/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'vitbot.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vitbot',
+	'USER': 'admin',
+	'PASSWORD': 'g41gnBD3h7TCI64',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
