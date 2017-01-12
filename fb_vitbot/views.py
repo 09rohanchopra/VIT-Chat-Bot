7177b972cd2be5ac2f64f114aa320ab53b89e1bf
Rohan Chopra <09rohanchopra@gmail.com>
2017-01-13 00:55:42 +0530

    Added typing inndicator

--- fb_vitbot/views.py
+++ fb_vitbot/views.py
@@ -24,17 +24,19 @@ class VitBotView(generic.View):
     def dispatch(self, request, *args, **kwargs):
         return generic.View.dispatch(self, request, *args, **kwargs)
 
+
     # Post function to handle Facebook messages
     def post(self, request, *args, **kwargs):
         # Converts the text payload into a python dictionary
         incoming_message = json.loads(self.request.body.decode('utf-8'))
         #logger.debug(incoming_message)
+	
         for entry in incoming_message['entry']:
             for message in entry['messaging']:
                 # Check if the received call is a message call
                 if 'message' in message:
                     # Print the message to terminal
-
+                    is_typing(self, message['sender']['id'],1)
                     if 'is_echo' in message['message']:
                         if message['message']['is_echo']:
                             pass
@@ -47,7 +49,21 @@ class VitBotView(generic.View):
                         # Check if the message has text, attachments don't generally have text
                         if 'text' in message['message']:   
                         	get_response(message['message']['text'],message['sender']['id'])
+        is_typing(self, message['sender']['id'],0)
         return HttpResponse()
+
+
+def is_typing(self, fbid, typing):
+        if typing == 1:
+            typing_indicator = "typing_on"
+        else:
+            typing_indicator = "typing_off"
+        post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=EAADljwd55ogBAJSRkD4JJZBklUPD7AKNb7g5FcTbZASrjJDBifAfz6y3OLJcAGQrYEcHhWAgfqduegl7rlL770u3xU21QTvzpVWtDsWajFguush2bDimEdorL4iT3ZC0kDz6G8khBzXbesxsgO7Spqrwm3aLbS26oCXATOPGAZDZD'
+
+
+        response_msg = json.dumps({"recipient":{"id":fbid}, "sender_action":typing_indicator})
+        status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
+
 
 
 def post_facebook_message(fbid, received_message):           
