98c3cd7e12fd38ca5f95c550cbb08fac593e5bc2
Rohan Chopra <09rohanchopra@gmail.com>
2017-01-12 15:20:19 +0530

    Added basic SQL model

--- fb_vitbot/models.py
+++ fb_vitbot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+	regno = models.CharField(max_length=9)
+	password = models.CharField(max_length=20)
+	data = models.CharField(max_length=1024)
